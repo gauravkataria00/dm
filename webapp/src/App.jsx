8996fee79615277d4636d619835b2af0cfddda3e
Himanshujangra400 <Jangrahimanshu076@gmail.com>
2026-03-14 20:54:50 +0530

    errro fix1

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,22 +1,12 @@
-// import AppRoutes from "./routes/AppRoutes";
-// import { ToastProvider } from "./context/ToastContext";
-
-// function App() {
-//   return (
-//     <ToastProvider>
-//       <AppRoutes />
-//     </ToastProvider>
-//   );
-// }
-
-// export default App;
+import AppRoutes from "./routes/AppRoutes";
+import { ToastProvider } from "./context/ToastContext";
 
 function App() {
   return (
-    <div style={{color:"black",fontSize:"30px"}}>
-      TEST APP WORKING
-    </div>
-  )
+    <ToastProvider>
+      <AppRoutes />
+    </ToastProvider>
+  );
 }
 
-export default App
+export default App;
