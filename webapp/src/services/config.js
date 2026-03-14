103ca857813b558c05c32b7feca723de1f71e28b
Himanshujangra400 <Jangrahimanshu076@gmail.com>
2026-03-14 16:50:34 +0530

    api problem solve

--- webapp/src/services/config.js
+++ webapp/src/services/config.js
@@ -1,7 +1,1 @@
-const isLocalhost =
-  window.location.hostname === "localhost" ||
-  window.location.hostname === "127.0.0.1";
-
-export const API_BASE_URL = isLocalhost
-  ? "http://localhost:5000"
-  : "https://dairy-backend-pw3n.onrender.com";
+export const API_BASE_URL = "https://dairy-backend-pw3n.onrender.com";
